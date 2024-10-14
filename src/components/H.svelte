577dcb4e380d9edeecaf9171b5d148a13cd973cf
Philix27 <philixbob@gmail.com>
2024-10-14 11:16:13 +0100

    install shadcn

---
+++ src/components/H.svelte
@@ -0,0 +1,8 @@
+<script>
+	//       h1: 'text-3xl font-extrabold',
+	//   h2: 'text-2xl font-bold',
+	//   h3: 'text-xl font-bold',
+	//   h4: 'text-lg font-bold',
+	//   h5: 'text-sm font-bold',
+	//   h6: 'text-xs font-medium',
+</script>
---
+++ src/components/P.svelte
@@ -0,0 +1,40 @@
+<script>
+	export let text;
+	export let className = '';
+	export let variant = 'p4';
+
+	const p1 = 'text-3xl font-extrabold leading-tight text-muted-foreground ' + className;
+	const p2 = 'text-2xl font-bold leading-tight text-muted-foreground ' + className;
+	const p3 = 'text-lg font-bold leading-tight  text-muted-foreground ' + className;
+	const p4 = 'text-md font-medium leading-tight text-muted-foreground ' + className;
+	const p5 = 'text-sm font-regular leading-tight text-muted-foreground ' + className;
+	const p6 = 'text-xs font-light leading-tight text-muted-foreground ' + className;
+</script>
+
+{#if variant == 'p1'}
+	<p class={p1}>{text}</p>
+{/if}
+
+{#if variant == 'p2'}
+	<p class={p2}>{text}</p>
+{/if}
+
+{#if variant == 'p3'}
+	<p class={p3}>{text}</p>
+{/if}
+
+{#if variant == 'p4'}
+	<p class={p4}>{text}</p>
+{/if}
+
+{#if variant == 'p5'}
+	<p class={p5}>{text}</p>
+{/if}
+
+{#if variant == 'p6'}
+	<p class={p6}>{text}</p>
+{/if}
+
+<style lang="postcss">
+
+</style>
