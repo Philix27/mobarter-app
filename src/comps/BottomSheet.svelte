96fc15cc8acdf92fd822b9fdfddbbb36db623cc3
Philix27 <philixbob@gmail.com>
2024-10-16 13:31:08 +0100

    added reusable components

---
+++ src/comps/BottomSheet.svelte
@@ -0,0 +1,45 @@
+<script>
+	import P from '../components/P.svelte';
+	import 'iconify-icon';
+
+	export let title = '';
+	export let show = false;
+	export let onClose = () => {};
+</script>
+
+{#if show}
+	<div
+		class="fixed top-0 left-0 bg-black/30 w-full bottom-0 z-30 h-screen flex flex-col justify-between"
+	>
+		<motion.div
+			initial={{ y: 2000 }}
+			animate={{ y: 0 }}
+			transition={{ ease: 'easeInOut', duration: 0.4 }}
+			class="bg-background w-full p-4 rounded-t-[20px] min-h-[calc(30vh)] border-t"
+		>
+			<div
+				class={`
+      w-full 
+      rounded-md
+      flex items-center justify-between
+      px-2 pb-2`}
+			>
+				{#if title}
+					<P>
+						{title}
+					</P>
+				{:else}
+					<div />
+				{/if}
+
+				<div class="rounded-[10px] p-[4px] bg-card" on:click={onClose} role="button" tabindex={1}>
+					<iconify-icon class="text-[15px]" icon="mdi:close" />
+				</div>
+			</div>
+			<hr class="" />
+			<div class="px-2 py-5">
+				<slot />
+			</div>
+		</motion.div>
+	</div>
+{/if}
---
+++ src/comps/Modal.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<div
+	class={`bg-black/50 w-full 
+      fixed left-0 z-10
+      top-0 mt-[50px]
+      h-screen
+      flex flex-col items-center
+      justify-center`}
+>
+	<div class="bg-background w-[90%] md:w-[50%] lg:w-[75%] rounded-lg px-4 py-4">
+		<slot />
+	</div>
+</div>
