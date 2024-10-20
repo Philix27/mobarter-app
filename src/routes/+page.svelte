1478a15422a24b15a60f6e091980d3de2eea524a
Philix27 <philixbob@gmail.com>
2024-10-20 16:36:30 +0100

    clean: remove web3 page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,13 @@
 	// import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
-	import Counter from './Counter.svelte';
+	import { account, chainId, modal } from 'lib/web3';
+
+	// import { connected } from 'svelte-wagmi';
+
+	function connectWallet() {
+		modal.open({ view: 'Connect' });
+	}
 </script>
 
 <svelte:head>
@@ -22,12 +28,15 @@
 		to your new<br />SvelteKit appex
 	</h1>
 
-	<Counter />
-
 	<a href="/dashboard" class="my-4"><p>Dashboard</p></a>
+
+	{#if $account.isConnected}
+		<p>Connected</p>
+	{:else}
+		<button on:click={connectWallet} class="p-2 bg-orange-500 rounded-lg">Not Connected</button>
+		<!-- <button on:click={}>Connect</button> -->
+	{/if}
 </section>
-
-
 
 <style>
 	section {
