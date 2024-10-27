a9f452f34cebbc0b26b0b6efdb7b0caa5726ca9f
Philix27 <philixbob@gmail.com>
2024-10-27 10:37:51 +0100

    fix: remove all prerendering

--- client/src/routes/+page.server.ts
+++ client/src/routes/+page.server.ts
@@ -1,14 +1,14 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
-// export const prerender = true;
+// export const prerender = false;
 
 import { redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
 
 export const actions: Actions = {
 	setTheme: async ({ url, cookies }) => {
-        const theme = url.searchParams.get('theme');
-        const redirectTo = url.searchParams.get("redirectTo")
+		const theme = url.searchParams.get('theme');
+		const redirectTo = url.searchParams.get('redirectTo');
 		console.log(theme);
 
 		if (theme) {
@@ -16,8 +16,8 @@ export const actions: Actions = {
 				path: '/',
 				maxAge: 60 * 60 * 24 * 365
 			});
-        }
-        
-        throw redirect(303, redirectTo ?? '/')
+		}
+
+		throw redirect(303, redirectTo ?? '/');
 	}
 };
